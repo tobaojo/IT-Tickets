fbf8558f9199f910646d7b6e9d8de65d6369d94b
Toba <toba_ojo@hotmail.com>
2022-11-24 10:37:37 +0000

    started work

---
+++ pages/components/MainBackground.tsx
@@ -0,0 +1,5 @@
+import Image from "next/image";
+
+const mainBackground = () => {
+    
+}
---
+++ pages/components/Navbar.tsx
@@ -0,0 +1,20 @@
+import styles from '../../styles/Home.module.css'
+
+const Navbar = () => {
+    return <nav className={styles.nav}>
+        <a href="http://" className={styles.title}>IT Tickets</a>
+        <ul>
+            <li>
+                <a href="http://">features</a>
+            </li>
+            <li>
+                <a href="http://">pricing</a>
+            </li>
+            <li>
+                <a href="http://">about</a>
+            </li>
+        </ul>
+    </nav>
+}
+
+export default Navbar
